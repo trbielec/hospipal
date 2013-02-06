bee716c92894bf8bfde086c065ffc884c4cdeab1
Ian Macaulay <ismacaul@gmail.com>
2013-02-05 22:33:52 -0700

    Added VS Project and Test Project

---
+++ Hospipal/HospipalTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HospipalTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
