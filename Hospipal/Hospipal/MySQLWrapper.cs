984242152095511b69fd0f0988c711f2edf61c16
Ian Macaulay <ismacaul@gmail.com>
2013-02-09 21:05:22 -0700

    Added MySQLWrapper.cs and MySQLWrapperTests.cs
    MySQLWrapper is the wrapper for the .net connector for MySQL. I also
    added unit tests for the class that all pass as of now.

---
+++ Hospipal/Hospipal/MySQLWrapper.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospipal
+{
+    using MySql.Data.MySqlClient;
+
+    public class MySQLWrapper
+    {
+        private MySqlConnection connection; // connection object to the database
+        private string server;              // server name
+        private string database;            // database name
+        private string userid;              // user id for database
+        private string password;            // password for user
+
+        /*
+        * Constructor: Initializes server, database, userid, and password variables and creates a MySQLConnection object
+        * 	@param server
+        *		- A string for the server of the DB
+        * 	@param database
+        * 		- A string for the database name
+        *	@param userid
+        *		- A string for the user id
+        *	@param password
+        *		- A string for the password
+        */
+        public MySQLWrapper(string server, string database, string userid, string password)
+        {
+            this.server = server;
+            this.database = database;
+            this.userid = userid;
+            this.password = password;
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + userid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        /*
+         * OpenConnection: Opens the connection to the database server, outputs an error message if one occurs
+         *  - returns true if successful, false otherwise
+         */
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                switch (e.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Error: Unable to connect to server");
+                        break;
+                    default:
+                        Console.WriteLine("MySQL Error " + e.Number + ":" + e.Message);
+                        break;
+                }
+                return false;
+            }
+        }
+
+        /*
+         * CloseConnection: Closes the connection to the database server
+         */
+        private void CloseConnection()
+        {
+            connection.Close();
+        }
+
+        /*
+         * Update: Used to run an UPDATE query
+         *  @param query
+         *      - The string value of the query
+         *  - Returns true if successful, false otherwise
+         */
+        public bool Update(string query)
+        {
+            return RunQueryWithNoReturn(query);
+        }
+
+        /*
+         * Insert: Used to run an INSERT query
+         *  @param query
+         *      - The string value of the query
+         *  - Returns true if successful, false otherwise
+         */
+        public bool Insert(string query)
+        {
+            return RunQueryWithNoReturn(query);
+        }
+
+        /*
+         * Select: Used to run an SELECT query
+         *  @param query
+         *      - The string value of the query
+         *  - Returns the values returned from the database as a List<object[]>
+         */
+        public List<object[]> Select(string query)
+        {
+            return RunQueryWithReturn(query);
+        }
+
+        /*
+         * Delete: Used to run an DELETE query
+         *  @param query
+         *      - The string value of the query
+         *  - Returns true if successful, false otherwise
+         */
+        public bool Delete(string query)
+        {
+            return RunQueryWithNoReturn(query);
+        }
+
+        /*
+         * RunQueryWithNoReturn: Runs the provided query string on the database.
+         *  @param query
+         *      - The string value of the query
+         *  - Returns true if the query is successful, false otherwise
+         */
+        private bool RunQueryWithNoReturn(string query)
+        {
+            if (OpenConnection())
+            {
+                try
+                {
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                    CloseConnection();
+                    return true;
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine("MySQL Error " + e.Number + ":" + e.Message);
+                    CloseConnection();
+                }
+            }
+
+            return false;
+        }
+
+        /*
+         * RunQueryWithReturn: Runs a query on the datbase that returns data
+         *  @param query
+         *      - The string value of the query
+         *	- Returns a List<object[]> where each element in the List is one row, null otherwise
+         */
+        private List<object[]> RunQueryWithReturn(string query)
+        {
+            if (OpenConnection())
+            {
+                try
+                {
+                    // Create SQL Command from query and connection object
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    // Execute command
+                    MySqlDataReader data = command.ExecuteReader();
+
+                    List<object[]> allData = new List<object[]>();
+                    // Loop through all returned objects from the DB and add them to the List
+                    // Each element in the list is an object[] containing all the data for each row
+                    while (data.Read())
+                    {
+                        object[] rowData = new object[data.FieldCount];
+                        for (int i = 0; i < rowData.Length; i++)
+                        {
+                            rowData[i] = data[i];
+                        }
+                        allData.Add(rowData);
+                    }
+
+                    // clean up
+                    data.Close();
+                    CloseConnection();
+                    return allData;
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine("MySQL Error " + e.Number + ":" + e.Message);
+                    CloseConnection();
+                }
+            }
+
+            return null;
+        }
+
+        /*
+         * TestConnection(): Tests the connection to the database server
+         *  - Returns true is successful, false otherwise
+         */
+        public bool TestConnection()
+        {
+            if (OpenConnection())
+            {
+                CloseConnection();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ Hospipal/HospipalTests/MySQLWrapperTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using MySql.Data.MySqlClient;
+using Hospipal;
+
+namespace HospipalTests
+{
+    [TestFixture]
+    public class MySQLWrapperTests
+    {
+        private MySQLWrapper wrapper;
+      
+        [TestFixtureSetUp]
+        public void setup() 
+        {
+        }
+
+        [TestCase]
+        public void TestTestConnectionMethidWithNullValues() 
+        {
+            wrapper = new MySQLWrapper(null, null, null, null);
+            Assert.False(wrapper.TestConnection());
+        }
+
+        [TestCase]
+        public void TestTestConnectionMethodWithInvalidServer()
+        {
+            wrapper = new MySQLWrapper("", "ismacaul_HospiPal", "ismacaul_seng403", "seng403");
+            Assert.False(wrapper.TestConnection());
+        }
+
+        [TestCase]
+        public void TestTestConnectionMethidWithInvalidDatabase() 
+        {
+            wrapper = new MySQLWrapper("s403g01.cpsc.ucalgary.ca", "ismacaul_hos", "ismacaul_seng403", "seng403");
+            Assert.False(wrapper.TestConnection());
+        }
+
+        [TestCase]
+        public void TestTestConnectionMethidWithInvalidUserID() 
+        {
+            wrapper = new MySQLWrapper("s403g01.cpsc.ucalgary.ca", "ismacaul_HospiPal", "seng403", "seng403");
+            Assert.False(wrapper.TestConnection());
+        }
+
+        [TestCase]
+        public void TestTestConnectionMethidInvalidPassword() 
+        {
+            wrapper = new MySQLWrapper("s403g01.cpsc.ucalgary.ca", "ismacaul_HospiPal", "ismacaul_seng403", "seng400");
+            Assert.False(wrapper.TestConnection());
+        }
+
+        [TestCase]
+        public void TestTestConnectionMethidWithValidValues() 
+        {
+            wrapper = new MySQLWrapper("s403g01.cpsc.ucalgary.ca", "ismacaul_HospiPal", "ismacaul_seng403", "seng403");
+            Assert.True(wrapper.TestConnection());
+        }
+
+        [TestCase]
+        public void TestQueryFunctionWithValidQueries()
+        {
+            wrapper = new MySQLWrapper("s403g01.cpsc.ucalgary.ca", "ismacaul_HospiPalTestDB", "ismacaul_seng403", "seng403");
+            string query = "INSERT INTO TestTable VALUES (1);";
+            Assert.True(wrapper.Insert(query));
+
+            query = "UPDATE TestTable SET TestColumn = 2 WHERE TestColumn = 1";
+            Assert.True(wrapper.Update(query));
+
+            query = "SELECT * FROM TestTable";
+            List<object[]> returnList = wrapper.Select(query);
+            object[] row = returnList[0];
+            int value = (int)row[0];
+            Assert.True(value == 2);
+
+            query = "DELETE FROM TestTable WHERE TestColumn = 2;";
+            Assert.True(wrapper.Delete(query));
+        }
+
+        [TestCase]
+        public void TestQueryFunctionsWithInvalidQueries()
+        {
+            wrapper = new MySQLWrapper("s403g01.cpsc.ucalgary.ca", "ismacaul_HospiPalTestDB", "ismacaul_seng403", "seng403");
+            string query = "INSERT INTO table VALUES (1);";
+            Assert.False(wrapper.Insert(query));
+
+            query = "UPDATE table SET TestColumn = 2 WHERE TestColumn = 1";
+            Assert.False(wrapper.Update(query));
+
+            query = "SELECT * FROM table";
+            List<object[]> returnList = wrapper.Select(query);
+            Assert.True(returnList == null);
+
+            query = "DELETE FROM table WHERE TestColumn = 2;";
+            Assert.False(wrapper.Delete(query));
+        }
+
+        [TestFixtureTearDown]
+        public void tearDown()
+        {
+        }
+    }
+}
