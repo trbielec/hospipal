5443ff014e829f61be8d6cbd7d92086c47fbe0cc
Brett Hamm <brett.hamm@gmail.com>
2013-02-20 21:48:12 -0700

    WARDS BEDS AND ROOMS

---
+++ Hospipal/Hospipal/UserControl_AddRoom.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hospipal
+{
+    /// <summary>
+    /// Interaction logic for UserControl_Ward.xaml
+    /// </summary>
+    public partial class UserControl_Ward : UserControl
+    {
+        public UserControl_Ward()
+        {
+            InitializeComponent();
+        }
+
+        private void RadButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
