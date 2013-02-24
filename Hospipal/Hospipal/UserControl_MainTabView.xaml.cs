2ebf6dc6564d2c1ac575b4faa8b6e279b7486a9e
Adesh Banvait <adesh911@gmail.com>
2013-02-24 13:37:44 -0700

    cimmomlgnsfoghs

--- Hospipal/Hospipal/UserControl_MainTabView.xaml.cs
+++ Hospipal/Hospipal/UserControl_MainTabView.xaml.cs
@@ -26,16 +26,22 @@ namespace Hospipal
             InitializeComponent();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
             PatientsTab.Content = new UserControl_PatientsView();
             EmployeesTab.Content = new UserControl_EmployeesView();
 =======
 <<<<<<< HEAD
+=======
+>>>>>>> 90d240e2611ec037221441026f6609960de4e81d
             WardsTab.Content = new UserControl_Wards();
 
 =======
             PatientsTab.Content = new UserControl_PatientsView();
             EmployeesTab.Content = new UserControl_EmployeesView();
 >>>>>>> Almost Final Changes
+<<<<<<< HEAD
+>>>>>>> 90d240e2611ec037221441026f6609960de4e81d
+=======
 >>>>>>> 90d240e2611ec037221441026f6609960de4e81d
         }
     }
