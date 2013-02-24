930f3a1ed17c46ab14eecff905d805150c9ca148
Julia Zochodne <julia.zochodne@gmail.com>
2013-02-24 16:36:30 -0700

    Adding functionality to Employee class.

--- Hospipal/Hospipal/Database Class/Employee.cs
+++ Hospipal/Hospipal/Database Class/Employee.cs
@@ -8,5 +8,60 @@ namespace Hospipal.Database_Class
 {
     class Employee
     {
+        private int _eid;
+        private string _fname;
+        private string _lname;
+        private string _specialty;
+
+        public Employee()
+        {
+            _fname = "";
+            _lname = "";
+            _specialty = "General";
+            GenerateEid();
+        }
+
+        public Employee(int eid, string fname, string lname, string specialty)
+        {
+            _eid = eid; 
+            _fname = fname;
+            _lname = lname;
+            _specialty = specialty; 
+        }
+
+        public int GetEid()
+        {
+            return _eid;
+        }
+
+        public void SetName(string first, string last)
+        {
+            _fname = first;
+            _lname = last; 
+        }
+
+        public List<string> GetName()
+        {
+            List<string> list = new List<string>();
+            list.Add(_fname);
+            list.Add(_lname);
+            return list;
+        }
+
+        public void SetSpecialty(string spec)
+        {
+            _specialty = spec; 
+        }
+
+        public string GetSpecialty()
+        {
+            return _specialty;
+        } 
+
+        private void GenerateEid()
+        {
+            List<object[]> obj = Database.Select("SELECT eid FROM ismacaul_HospiPal.Employee");
+            _eid = obj.Count() + 1;
+        }
     }
 }
