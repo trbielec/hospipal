e1a4f23d78a870961df580826f7ae9f8aa9b081f
Ian Macaulay <ismacaul@gmail.com>
2013-02-26 16:33:46 -0700

    Added Test Classes for all database classes

---
+++ Hospipal/HospipalTests/RoomClassTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+namespace HospipalTests
+{
+    [TestFixture]
+    public class RoomClassTests
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Hospipal/HospipalTests/WardClassTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+namespace HospipalTests
+{
+    [TestFixture]
+    public class WardClassTests
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+        }
+    }
+}
