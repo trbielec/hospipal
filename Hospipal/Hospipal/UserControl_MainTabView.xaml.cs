2ee5b9f1ff173d397820522ace71d0563e92d3ad
Tom Bielecki <tom.bielecki@gmail.com>
2013-03-10 18:31:08 -0600

    Notifications Functionality Added
    A text box has been added to the bottom of the Main Tab View, which
    polls the database every 5 seconds for new information. If you edit this
    box and then remove keyboard focus (Tab key), it will update the
    database.
    
    Tweaking these values might be needed in the future for better user
    experience. But it works as described above.

--- Hospipal/Hospipal/UserControl_MainTabView.xaml.cs
+++ Hospipal/Hospipal/UserControl_MainTabView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Telerik.Windows.Controls;
 
 namespace Hospipal
@@ -21,6 +23,16 @@ namespace Hospipal
     /// </summary>
     public partial class UserControl_MainTabView : UserControl
     {
+        Database_Class.Notification Notif = new Database_Class.Notification();
+        private static readonly DependencyProperty NotificationProperty =
+                          DependencyProperty.Register("notification", typeof(string),
+                                                      typeof(UserControl_MainTabView));
+        private string notification
+        {
+            get { return (string)GetValue(NotificationProperty); }
+            set { SetValue(NotificationProperty, value); }
+        }
+
         public UserControl_MainTabView()
         {
             InitializeComponent();
@@ -28,6 +40,25 @@ namespace Hospipal
             PatientsTab.Content = new UserControl_PatientsView();
             EmployeesTab.Content = new UserControl_EmployeesView();
             WardsTab.Content = new UserControl_Wards();
+
+            DispatcherTimer _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(5000); //5 seconds
+            _timer.Tick += new EventHandler(delegate(object s, EventArgs a)
+            {
+                Notif.RetrieveNotification();
+                notification = Notif.Text;
+            });
+            _timer.Start();
+
         }
+
+        #region event handlers
+        private void Notifications_Bar_LostFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            Notif.SendNotification(notification);
+            //Should it retrieve the notification after sending?
+            //Notif.RetrieveNotification();
+        }
+        #endregion
     }
 }
