edbdf38bd4b60b665e889e3ff007ce075ae12417
Ian Macaulay <ismacaul@gmail.com>
2013-03-19 17:48:40 -0600

    Fixing Merge Issues

---
+++ Hospipal/HospipalTests/NotificationClassTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using Hospipal.Database_Class;
+using System.Collections.Generic;
+using Hospipal;
+
+namespace HospipalTests
+{
+    [TestFixture]
+    public class NotificationClassTests
+    {
+        [TestCase]
+        public void TestRetreiveNotification()
+        {
+            Notification n = new Notification();
+            // Save old text if any exists
+            n.RetrieveNotification();
+            string oldText = n.Text;
+
+            n.Text = "Test Text";
+            n.SendNotification();
+            n.RetrieveNotification();
+            
+            Assert.True(n.Text.Equals("Test Text"));
+
+            n.Text = oldText;
+            n.SendNotification();
+        }
+
+        [TestCase]
+        public void TestSendNotification()
+        {
+            Notification n = new Notification();
+            // Save old text if any exists
+            n.RetrieveNotification();
+            string oldText = n.Text;
+
+            n.Text = "Test Text";
+            n.SendNotification();
+
+            string s = Database.Select("SELECT * from Notification")[0][1].ToString();
+            Assert.True(n.Text.Equals(s));
+
+            n.Text = oldText;
+            n.SendNotification();
+        }
+    }
+}
