7a7cd72259ad103899512cffd4f22d44ca86f819
Winston <winston.chan@live.com>
2013-04-04 16:29:15 -0600

    Merge Login

--- Hospipal/Hospipal/Database Class/Login.cs
+++ Hospipal/Hospipal/Database Class/Login.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Data.SqlClient;
-using System.Security;
+using System.Security.Cryptography;
 
 namespace Hospipal.Database_Class
 {
@@ -34,23 +34,83 @@ namespace Hospipal.Database_Class
             return fieldsFromDatabase;
         }
 
-        public static bool ValidateUser(string userName, string password)
+                public static void CreateLogin(int eid, string fname, string lname)
         {
-            int hash;
-            int salt;
-            int eid;
-            int login;
-
-            bool validated = false;
-            List<string> fieldsFromDatabase = Login.GetUserData(userName);
-
-            hash = Convert.ToInt32(fieldsFromDatabase[1]);
-            salt = Convert.ToInt32(fieldsFromDatabase[2]);
-            eid = Convert.ToInt32(fieldsFromDatabase[3]);
-            login = Convert.ToInt32(fieldsFromDatabase[4]);
-
-            return validated;
+            
         }
+
+        public static bool VerifyLogin(string userName, string password)
+        {
+            MySqlCommand login = new MySqlCommand("Select_Login(@userName);");
+            login.Parameters.AddWithValue("userName", userName);
+
+            List<object[]> loginInfo = Database.Select(login);
+
+            if (loginInfo == null || loginInfo.Count() != 1)
+            {
+                return false;
+            }
+
+            byte[] hashedPassword = (byte[])loginInfo[0][1];
+            byte[] salt = (byte[])loginInfo[0][2];
+
+            return ValidatePassword(password, hashedPassword, salt);
+            
+        }
+
+        /// Salted password hashing with PBKDF2-SHA1.
+        /// Adapted for our needs from: www: http://crackstation.net/hashing-security.htm
+        private const int SALT_BYTES = 24;
+        private const int HASH_BYTES = 24;
+        private const int PBKDF2_ITERATIONS = 1000;
+
+        public static byte[] CreateHash(string password, byte[] salt)
+        {
+            // Hash the password and encode the parameters
+            return Hash(password, salt);
+        }
+
+        public static byte[] CreateSalt()
+        {
+            // Create the salt
+            RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[SALT_BYTES];
+            rand.GetBytes(salt);
+
+            return salt;
+        }
+
+        private static bool ValidatePassword(string password, byte[] storedHash, byte[] salt)
+        {
+            // Create test hash from salt and password provided and check
+            byte[] testHash = Hash(password, salt);
+            return SlowEquals(storedHash, testHash);
+        }
+
+        private static string CreateUsername(string fname, string lname)
+        {
+            return null;
+        }
+
+        // Compares two byte arrays in length-constant time. This comparison
+        // method is used so that password hashes cannot be extracted from
+        // on-line systems using a timing attack and then attacked off-line.
+        private static bool SlowEquals(byte[] a, byte[] b)
+        {
+            uint diff = (uint)a.Length ^ (uint)b.Length;
+            for (int i = 0; i < a.Length && i < b.Length; i++)
+                diff |= (uint)(a[i] ^ b[i]);
+            return diff == 0;
+        }
+
+        // Hash the password with the salt
+        private static byte[] Hash(string password, byte[] salt)
+        {
+            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+            pbkdf2.IterationCount = PBKDF2_ITERATIONS;
+            return pbkdf2.GetBytes(HASH_BYTES);
+        }
+    
     }
 
 }
